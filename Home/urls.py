32475f64dad8fbc30315b90a0badc2e18f9b3f67
MengSonly17 <msonly114@gmail.com>
2025-08-27 22:44:29 +0700

    v3.0

--- Home/urls.py
+++ Home/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.base,name='base'),
-    path('index',views.index,name='index'),
+    path('', views.index, name='index'),
+    path('shop', views.shop, name='shop'),
+    path('productdetails', views.productdetails, name='product_details'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
+    path('shop', views.shop, name='shop'),
+    path('checkout', views.checkout, name='checkout'),
+    path('cart', views.cart, name='cart'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,8 +1,52 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-def base(request):
-    return render(request, 'base.html')
+import requests
+from django.templatetags.static import static
 
 def index(request):
-    return HttpResponse('Hello World!',status=200)
+    context = {'title': 'Index'}
+    return render(request, 'index.html',context)
+
+def shop(request):
+    url = static('data/product.json')
+    full_url = f"http://localhost:8000/{url}"
+
+    response = requests.get(full_url)
+    data = response.json()
+
+    context = {'title': 'Shop','data':data }
+
+    return render(request, 'shop.html',context)
+
+def productdetails(request):
+    context = {'title': 'ProductDetails'}
+
+    return render(request, 'productdetail.html', context)
+
+def contact(request):
+    context = {'title': 'Contact'}
+    return render(request,'contact.html',context)
+
+def about(request):
+    context = {'title': 'About'}
+    return render(request,'aboutus.html',context)
+
+def checkout(request):
+    context = {'title': 'Checkout'}
+    return render(request,'checkout.html',context)
+
+def cart(request):
+    context = {'title': 'Cart'}
+    return render(request,'cart.html',context)
+
+
+
+
+
+
+
+
+
+
+
+
+
